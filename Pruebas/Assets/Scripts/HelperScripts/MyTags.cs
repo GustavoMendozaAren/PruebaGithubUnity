f3ba114eb65f7f051ddacb7ffd10ba4c7e7927a6
GustavoMendozaAren <gmendoza10@alumnos.uaq.mx>
2022-09-21 15:08:01 -0500

    Merge remote-tracking branch 'origin/main' into main

---
+++ Pruebas/Assets/Scripts/HelperScripts/MyTags.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTags : MonoBehaviour
+{
+    public static string WALK_ANIMATION = "Walk";
+
+    public static string WALK_TRIGGER = "Walk";
+    public static string STOP_TRIGGER = "Stop";
+
+    public static string PLAYER_TAG = "Player";
+}
