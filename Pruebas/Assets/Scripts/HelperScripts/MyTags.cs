a340294069fa08b45bdbccb5fb740454c37f38a9
GustavoMendozaAren <gmendoza10@alumnos.uaq.mx>
2022-09-22 19:30:31 -0500

    Sharkmov

--- Pruebas/Assets/Scripts/HelperScripts/MyTags.cs
+++ Pruebas/Assets/Scripts/HelperScripts/MyTags.cs
@@ -8,6 +8,7 @@ public class MyTags : MonoBehaviour
 
     public static string WALK_TRIGGER = "Walk";
     public static string STOP_TRIGGER = "Stop";
+    public static string ATTACK_TRIGGER = "Attack";
 
     public static string PLAYER_TAG = "Player";
 }
