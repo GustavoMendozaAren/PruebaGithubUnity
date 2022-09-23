9621a52e3f2fe2d6210525fc64fe534188fd856a
GustavoMendozaAren <gmendoza10@alumnos.uaq.mx>
2022-09-22 19:51:53 -0500

    Shark

--- Pruebas/Assets/Scripts/HelperScripts/MyTags.cs
+++ Pruebas/Assets/Scripts/HelperScripts/MyTags.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MyTags : MonoBehaviour
 {
     public static string WALK_ANIMATION = "Walk";
+    public static string ATTACK_ANIMATION = "Attack";
 
     public static string WALK_TRIGGER = "Walk";
     public static string STOP_TRIGGER = "Stop";
